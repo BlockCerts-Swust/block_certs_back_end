77a13232460739ec4d87028068d1419485485d25
Jess <2482003411@qq.com>
2020-04-12 16:23:14 +0800

    证书发布接口

--- schools/apiviews.py
+++ schools/apiviews.py
@@ -8,15 +8,22 @@
 @Version   : 1.0
 @Description: 
 """
+import logging
 
+from django.db.models import QuerySet
+from rest_framework_mongoengine.viewsets import GenericViewSet
+
+logging.getLogger().setLevel(logging.INFO)
 from rest_framework import generics, status, viewsets
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import BasePermission
 from rest_framework.response import Response
 from rest_framework.settings import APISettings, DEFAULTS, IMPORT_STRINGS
 from rest_framework.views import APIView
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view, authentication_classes
 from schools.auth import SchoolAuthentication
+from schools.cert_issuer import config
+from schools.cert_issuer.issue_certificates import main
 from schools.serializers import SchoolSerializer, RevocationSerializer
 from schools.models import School, SchoolToken, Revocation
 from students.hashers import check_password
@@ -270,7 +277,7 @@ class RevocationView(APIView):
     def perform_destroy(self, instance):
         instance.delete()
 
-class CertSchoolDetailViewSet(viewsets.ModelViewSet):
+class CertSchoolDetailViewSet(GenericViewSet):
     authentication_classes = (SchoolAuthentication, )
     permission_classes = (BasePermission,)
     serializer_class = CertDetailSerializer
@@ -292,17 +299,119 @@ class CertSchoolDetailViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
+class CertIssueViewSet(viewsets.ModelViewSet):
+    authentication_classes = (SchoolAuthentication,)
+    permission_classes = (BasePermission,)
+    serializer_class = CertSerializer
+    queryset = Cert.objects.all()
+    lookup_field = 'cert_id'
+
     def create(self, request, *args, **kwargs):
         pass
 
     def retrieve(self, request, *args, **kwargs):
-        pass
+        instance = self.get_object()
+        if instance is None:
+            return Response({"code": 1001, "msg": "操作失败", "data": {"err": "证书不存在"}},
+                            status=status.HTTP_401_UNAUTHORIZED,
+                            content_type="application/json")
+        if instance.school_pubkey != "ecdsa-koblitz-pubkey:" + self.request.user.public_key:
+            return Response({"code": 1001, "msg": "操作失败", "data": {"err": "没有权限, 您不是该证书的颁发者"}},
+                            status=status.HTTP_401_UNAUTHORIZED,
+                            content_type="application/json")
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
     def update(self, request, *args, **kwargs):
         pass
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
     def destroy(self, request, *args, **kwargs):
         pass
 
-class CertIssueViewSet(viewsets.ModelViewSet):
-    pass
+    @action(methods=['POST'], detail=True, url_path='issue', url_name='cert-issue')
+    def cert_issue(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance is None:
+            return Response({"code": 1001, "msg": "操作失败", "data": {"err": "证书不存在"}},
+                            status=status.HTTP_400_BAD_REQUEST,
+                            content_type="application/json")
+        if instance.school_pubkey != "ecdsa-koblitz-pubkey:" + self.request.user.public_key:
+            return Response({"code": 1001, "msg": "操作失败", "data": {"err": "没有权限, 您不是该证书的颁发者"}},
+                            status=status.HTTP_401_UNAUTHORIZED,
+                            content_type="application/json")
+        cert_info = CertDetail.objects.filter(wsid=instance.cert_id).first()
+        if cert_info is None:
+            return Response({"code": 1001, "msg": "操作失败", "data": {"err": "获取证书详细信息失败"}},
+                            status=status.HTTP_401_UNAUTHORIZED,
+                            content_type="application/json")
+        try:
+            base_config = request.data
+            base_config["unsigned_certificates"] = {cert_info.unsign_cert["id"]: cert_info.unsign_cert}
+            parsed_config = config.get_config(base_config)
+            tx_id, certificates_to_issue = main(parsed_config)
+            if tx_id:
+                logging.info('Transaction id is %s', tx_id)
+                logging.info('certificates_to_issue %s', certificates_to_issue)
+                instance.txid = tx_id
+                instance.status = 1
+                instance.save()
+                for _, metadata in certificates_to_issue.items():
+                    cert_info_data = {"block_cert": metadata.blockcert}
+                    cert_info.update(**cert_info_data)
+                    break
+                return Response({
+                    "code": 1000, "msg": "操作成功", "data": self.get_serializer(instance).data},
+                    status=status.HTTP_200_OK,
+                    content_type="application/json"
+                )
+            else:
+                logging.error('Certificate issuing failed')
+                return Response({"code": 1001, "msg": "操作失败", "data": {"err": "证书颁发失败"}},
+                                status=status.HTTP_400_BAD_REQUEST,
+                                content_type="application/json")
+
+        except Exception as ex:
+            logging.error(ex, exc_info=True)
+            return Response({"code": 1001, "msg": "操作失败", "data": {"err": "证书颁发失败"}},
+                            status=status.HTTP_400_BAD_REQUEST,
+                            content_type="application/json")
+
+
+    def get_queryset(self):
+        """
+        Get the list of items for this view.
+        This must be an iterable, and may be a queryset.
+        Defaults to using `self.queryset`.
+
+        This method should always be used rather than accessing `self.queryset`
+        directly, as `self.queryset` gets evaluated only once, and those results
+        are cached for all subsequent requests.
+
+        You may want to override this if you need to provide different
+        querysets depending on the incoming request.
+
+        (Eg. return a list of items that is specific to the user)
+        """
+        assert self.queryset is not None, (
+                "'%s' should either include a `queryset` attribute, "
+                "or override the `get_queryset()` method."
+                % self.__class__.__name__
+        )
+
+        queryset = self.queryset
+        if isinstance(queryset, QuerySet):
+            # Ensure queryset is re-evaluated on each request.
+            user = self.request.user
+            queryset = queryset.filter(school_pubkey="ecdsa-koblitz-pubkey:" + user.public_key).all()
+        return queryset
